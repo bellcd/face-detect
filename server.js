1d7d42d75ed114559af033ab370c54f41b64f852
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-29 16:40:22 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+app.use(cors())
+
+app.use(express.static('dist'));
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>The app renders.</div>
+    );
+  }
+}
+
+export default App;
