945993a43073137844def45dd49b4682b04032ae
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-12-01 20:51:24 -0500

    Moves express and cors to runtime dependencies

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors())
+app.use(cors());
 
 app.use(express.static('dist'));
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
